60ec0198a5ae21006e69a6704f59862a303ae922
niccesvahn <49589618+NicceSvahn@users.noreply.github.com>
2019-06-05 03:04:23 +0200

    Created a simple gameloop
    Added a set framerate limit which makes the gameloop update every 1/60 second.

--- Project2/Header Files/game.h
+++ Project2/Header Files/game.h
@@ -8,4 +8,6 @@ public:
 	void run();
 private:
 	void update();
+	sf::RenderWindow mWindow;
+	static const int FRAMES_PER_SECOND = 60;
 };
--- Project2/Source Files/game.cpp
+++ Project2/Source Files/game.cpp
@@ -1,12 +1,42 @@
 #pragma once
-#include "game.h"
+#include "../Header Files/game.h"
+#include <iostream>
 
-	static const int mWindowHeight = 1300;
-	static const int mWindowWidth = 1500;
+	static const int mWindowHeight = 600;
+	static const int mWindowWidth = 800;
 
 	Game::Game() :
-		mWindow(sf::VideoMode(mWindowWidth, mWindowHeight), "Platformer");
-	}
+		mWindow(sf::VideoMode(mWindowWidth, mWindowHeight), "Platformer"){}
 
 	Game::~Game() {}
+
+	void Game::run() {
+		mWindow.setFramerateLimit(FRAMES_PER_SECOND);
+
+		while (mWindow.isOpen()) {
+
+			sf::Event event;
+			while (mWindow.waitEvent(event)) {
+
+				if (event.type == sf::Event::Closed)
+					mWindow.close();
+
+				// Update 
+
+				// Clear canvas
+
+				// Render
+
+				// Game logic (collision)
+
+				// Input detection
+
+
+			}
+		}
+	}
+
+	void Game::update() {
+
+	}
 
